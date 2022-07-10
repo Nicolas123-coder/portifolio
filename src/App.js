942682335091be5317f82b3ad79a170bf9e9e230
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2022-07-09 21:46:55 -0300

    Services component completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import Particles from 'react-tsparticles';
 import NavBar from './components/Navbar';
 import Header from './components/Header';
-import AboutMe from './components/AboutMe'
+import AboutMe from './components/AboutMe';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <NavBar/>
     <Header/>
     <AboutMe/>
+    <Services/>
     </>
     );
 }
