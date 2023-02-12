422a9b994b896980b84986071cbf4a515d7aeb40
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2023-02-11 22:21:28 -0300

    Building a Portfolio section

---
+++ src/components/Portfolio.js
@@ -0,0 +1,50 @@
+import React from "react";
+import netflix from "../images/netflix.png";
+import cityGuide from "../images/city-guide-app.png";
+import portfolio from "../images/portfolio.png";
+import taskManager from "../images/task-manager.png";
+// FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+// REACT POPUPBOX
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css";
+
+
+const Portfolio = () => {
+
+  return (
+    <div className="portfolio-wrapper">
+      <div className="container">
+        <h1 className="text-uppercase text-center py-5">portfolio</h1>
+        <div className="d-flex justify-content-md-center">
+          <div className="portfolio-image-box" onClick={() => window.open("https://nicolas123-coder.github.io/monster-encyclopedia/", "_blank")}>
+            <img className="portfolio-image" src={netflix} alt="Netflix Clone Project..." />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+          {/* - */}
+          <div className="portfolio-image-box" onClick={() => window.open("https://nicolas123-coder.github.io/monster-encyclopedia/", "_blank")}>
+            <img className="portfolio-image" src={cityGuide} alt="City Guide Project..." />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+          {/* - */}
+          <div className="portfolio-image-box" onClick={() => window.open("https://nicolas123-coder.github.io/monster-encyclopedia/", "_blank")}>
+            <img className="portfolio-image" src={portfolio} alt="Portfolio React and Material UI Project..." />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+          {/* - */}
+          <div className="portfolio-image-box" onClick={() => window.open("https://nicolas123-coder.github.io/monster-encyclopedia/", "_blank")}>
+            <img className="portfolio-image" src={taskManager} alt="Task Manager React and Redux Project..." />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio;
